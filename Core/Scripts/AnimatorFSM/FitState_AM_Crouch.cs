7b0f0446524979b2cf41766d823139fb147f0370
BlankMauser <thomasluvan@gmail.com>
2016-02-05 13:32:09 -0600

    Added Crouch Animation

--- Core/Scripts/AnimatorFSM/FitState_AM_Crouch.cs
+++ Core/Scripts/AnimatorFSM/FitState_AM_Crouch.cs
@@ -1,15 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using FSMHelper;
 
-public class FitState_AM_Crouch : MonoBehaviour {
+public class FitState_AM_Crouch : BaseFSMState
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+		RayCastColliders controller;
+		public Animation anim; 
+
+		public override void Enter()
+		{
+				FitAnimatorStateMachine SM = (FitAnimatorStateMachine)GetStateMachine();
+				controller = SM.m_GameObject.GetComponent<RayCastColliders>();
+				controller.state = CharacterState.CROUCH;
+				anim = controller.anima;
+				anim.Play ("Idle");
+				controller.ApplyFriction = true;
+				controller.C_Drag = controller.movement.friction;
+		}
+
+		public override void Exit()
+		{
+				controller.previousState = CharacterState.CROUCH;
+		}
+
+		public override void Update()
+		{
+				//				controller.Inputter.GetInput ();
+				//				controller.Inputter.ProcessInput ();
+
+				if (controller.BfAction == BufferedAction.JAB) {
+						DoTransition (typeof(FitState_AM_GroundAttack));
+						return;
+				}
+				if (controller.BfAction == BufferedAction.JUMP) {
+
+						DoTransition (typeof(FitState_AM_JumpSquat));
+						return;
+				}
+				if (controller.IsGrounded (controller.groundedLookAhead) == false) {
+						DoTransition (typeof(FitState_AM_Fall));
+						return;
+				}
+
+				if (controller.BfAction == BufferedAction.WALKING) {
+						if (Mathf.Abs (controller.Inputter.x) >= 0.5f) {
+								DoTransition (typeof(FitState_AM_WalkFast));
+								return;
+						} else {
+								DoTransition (typeof(FitState_AM_WalkSlow));
+								return;
+						}
+				}
+
+				if (controller.BfAction == BufferedAction.INIT_DASH) {
+						DoTransition (typeof(FitState_AM_InitDash));
+						return;
+				}
+
+		}
+
+
 }
